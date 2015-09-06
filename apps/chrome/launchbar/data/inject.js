e964932cd209f7c5fcbb3c3de22082c16b5c12b2
Dan Borufka <d.borufka@pjureisobar.com>
2015-09-06 20:47:09 +0200

    Chrome Extension: added injection

---
+++ apps/chrome/launchbar/data/inject.js
@@ -0,0 +1,18 @@
+
+chrome.storage.sync.get(
+{
+	base_path:          'https://danborufka.github.io/cdn/launchbar-js/',
+	autoload:           true,
+	user_command_path:  '',
+	shortcut:           'ctrl + space'
+}, 
+function(items) 
+{
+	var base_path			= items.base_path,
+		autoload 			= items.autoload,
+		user_command_path 	= items.user_command_path,
+		shortcut 			= items.shortcut;
+
+	// ### inject:
+	// base_path + "/js/bookmarklet.min.js";
+});
