56c8dace296bd89890f5497b71fe378a628ddef4
Dan Borufka <d.borufka@pjureisobar.com>
2015-09-08 23:11:33 +0200

    Banner++

--- commands/banner.js
+++ commands/banner.js
@@ -9,50 +9,53 @@ LAUNCHBAR.install({
 					AD_NETWORKS.GDN =
 					{
 						name: 	"GDN Network",
-						tags:	[ "meta[name=\"ad.size\"][content]" ],
+						tags:	[ 'meta[name="ad.size"]' ],
 						css: 	[ "border" ],
-						checks:	function()
+						checks:	function(check)
 								{
-									var check 		= true;
+									$("script[src*=edge]").each(function()
+									{
+										check &= $(this).attr('src').indexOf('adobe.com/runtime') < 0;
+									});
+									if(!check) return alert('Edge runtime isn\'t included using a relative path');
 									
-									check &= $("script[src*=edge]").attr('src').indexOf('adobe.com/runtime') < 0;
-									if(!check) alert('Edge runtime isn\'t included using a relative path');
-									
-									$.ajax('index_edge.js')
+									$.get('index_edge.js')
 										.done(function(data)
 										{
-											var check = (data.indexOf('window.open(') < 0) && (data.indexOf('clicktag') < 0);
-											if(!check) alert('No clicktag defined!');
+											var check = data.indexOf('clicktag') > 0;
+											if(!check) 	alert('Warning, no clicktag defined!');
 										});
 
 									return check;
 								}
 					};
 					
-					var netw = AD_NETWORKS[slug],
-						check = true,
-						$stage = $('#Stage'),
-						size = { width: $stage.width(), height: $stage.height()  };
+					var netw 	= AD_NETWORKS[slug],
+						check 	= true,
+						$stage 	= $('#Stage'),
+						size 	= { width: $stage.width(), height: $stage.height() };
 
 					netw.css.forEach(function(css)
 					{
 						var $match = $stage.find('[style*=' + css + ']');
 
+						//only if found and (almost) matches stage size
 						check &= 	$match.length && 	
 									Math.abs(size.width - $match.width())   < 4 && 
 									Math.abs(size.height - $match.height()) < 4;
 
-						if(!check) alert(css + ' is missing!');
+						if(!check) return alert(css + ' is missing!');
 					});
 
 					netw.tags.forEach(function(tag)
 					{
-						console.log(tag, $(tag));
 						check &= !!$(tag).length;
-						if(!check) alert('HTML Tag is missing: <' + tag + '>');
+						if(!check) return alert('HTML Tag is missing: <' + tag + '>');
 					});
 
-					if(check) alert('Banner is ready for ' + netw.name + '!');
+					check = netw.checks(check);
+
+					if(check) alert('Banner is ready for '+netw.name + '!');
 				}
 	},
 	labels:
