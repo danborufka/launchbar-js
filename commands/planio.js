12708a50f188146117846c5b9e16d0c5353e0192
Dan Borufka <d.borufka@pjureisobar.com>
2015-09-09 12:06:51 +0200

    Planio mit Userlogin

--- commands/planio.js
+++ commands/planio.js
@@ -1,8 +1,28 @@
+var user = { name:'Dan', pass:'danplan10' };
+
 $(".updated_on")
 	.after('<td><input class="lb_done" type="checkbox" /></i></td>');
+
+$('table.list.issues thead th:last').after('<th>Erledigt</th>');
 
 $(document)
 	.on('click', '.lb_done', function()
 	{
-		alert('Speicheeeern!');
+		var $row 	= $(this).parentsUntil('tr').parent(),
+			id 		= parseInt($row[0].id.split('-')[1]);
+
+		$.ajax({
+			username: 	user.name,
+			password: 	user.pass,
+			dataType: 	'json',
+			async: 		false,
+			url: 		'/issues/' + id + '.json',
+			method: 	'PUT',
+			data: 		{ issue: { id: id, status_id: 5, notes: 'Würde ich als erledigt erachten.' } }
+		}).done(function()
+		{
+			console.log('say whaaat');
+		});
+
+		$row.fadeOut(300);
 	});
